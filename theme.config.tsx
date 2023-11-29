46f2cc1a3b3ae47a42e4674e43429ccc22cf2094
Fludem <m40a3@hotmail.co.uk>
2023-11-29 02:24:31 +0000

    Remove github and discord

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,15 +3,8 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Workethyc</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Workethyc Docs',
   },
 }
 
